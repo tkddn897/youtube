2e7e99bdf5916c6b6c9da68ac536629235125a2d
tkddn897 <tkddn5221@naver.com>
2020-12-14 22:16:36 +0900

    Updates

---
+++ precache-manifest.8eb501be8cdc607b151a1f2ab617f978.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff91fb064065cb1836fa35e95bd3ae0e",
+    "url": "/youtube/index.html"
+  },
+  {
+    "revision": "99e82e071f3195d20685",
+    "url": "/youtube/static/css/main.7d79d167.chunk.css"
+  },
+  {
+    "revision": "591875316961e9dd6eb3",
+    "url": "/youtube/static/js/2.298d6e5b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/youtube/static/js/2.298d6e5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99e82e071f3195d20685",
+    "url": "/youtube/static/js/main.a109ebf6.chunk.js"
+  },
+  {
+    "revision": "3cb9ec445f1b9d74a694",
+    "url": "/youtube/static/js/runtime-main.9d3efbef.js"
+  }
+]);
